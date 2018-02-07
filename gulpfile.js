f41d241cf6d8f5b7153de97827d0a8032204c95f
loicbourgois <loic@bourgois.pro>
2018-02-08 00:23:23 +0100

    lint

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require("gulp");
+var lintspaces = require("gulp-lintspaces");
+
+gulp.task("lint", function() {
+	return gulp.src("./src/**/*.java")
+		.pipe(lintspaces({
+			trailingspaces: true,
+			indentation: 'tabs',
+			newlineMaximum: 2
+		}))
+		.pipe(lintspaces.reporter());
+});
