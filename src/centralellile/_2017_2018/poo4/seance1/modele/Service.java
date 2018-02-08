66643d77ebea764a6e0e5a4486137e37d6927eea
loicbourgois <loic@bourgois.pro>
2018-02-08 07:54:33 +0100

    Question 5

--- src/centralellile/_2017_2018/poo4/seance1/modele/Service.java
+++ src/centralellile/_2017_2018/poo4/seance1/modele/Service.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2018 Team 2i.
+ * Copyright 2018 Team SI.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
